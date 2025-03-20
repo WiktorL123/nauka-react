6af8096a2c197082a5ee82c4f28fda5de5776e8c
wiktor <wiktorlem1@wp.pl>
2025-03-20 20:11:23 +0100

    dodawanie uzytkownikow oraz zapisywanie ich do localStorage - dodalem odpowiednie komentarze zebys sie nie zgubil

--- users/src/components/AddUserForm.jsx
+++ users/src/components/AddUserForm.jsx
@@ -1,7 +1,8 @@
 import * as Yup from "yup";
 import {useFormik} from "formik";
+import {useState} from "react";
 
-export default function AddUserForm(){
+export default function AddUserForm({handleAddUser}) {
 
 
     const formik = useFormik({
@@ -23,28 +24,67 @@ export default function AddUserForm(){
         onSubmit: (values) => {
              console.log(values);
              alert('dodano uzytkownika ')
+            handleAddUser({
+                id: Date.now(),
+                name: values.name,
+                username: values.username,
+                email: values.email,
+                address: {street: 'lazienkowska'}, //ustawiamy domyslne wartosci dla ulicy i numeru teleofoonu zeby userlist nie beczal
+                phone: '999-999-999'
+            });
         }
     })
-
-
-
-
 
 
 
     return (
         <>
             <h1>Formularz dodawania nowego użytkownika</h1>
-            <form onSubmit={formik.handleSubmit}>
+            <form
+                className={'flex-col flex  '}
+                onSubmit={formik.handleSubmit}
+            >
                 <input
+                    className={'border-1 my-2'}
                     type="text"
                     name="name"
                     placeholder='Santiago'
                     value={formik.values.name}
-                    onChange={}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+
                 />
 
-                {formik.touched.name && formik.errors.name  && ( <p>{formik.errors.name}</p>)}
+                {formik.touched.name && formik.errors.name  &&  <p className={'text-red-500'}>{formik.errors.name}</p>}
+
+
+                <input
+                    className={'border-1 my-2'}
+                    type='text'
+                    name='username'
+                    placeholder='Chile'
+                    value={formik.values.username}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                />
+                {formik.touched.username && formik.errors.username && <p className={'text-red-500'}>{formik.errors.username}</p>}
+
+                <input
+                    className={'border-1 my-2'}
+                    type={'email'}
+                    name='email'
+                    placeholder='Santiago@gmail.com'
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                />
+                {formik.touched.email && formik.errors.email && <p className={'text-red-500'}>{formik.errors.email}</p>}
+
+                <button
+                    className={'rounded-xl my-2 bg-blue-400 hover:bg-blue-500 hover:text-white transition-all duration-300 hover:scale-105 cursor-pointer'}
+                    disabled = {!formik.values.name|| !formik.values.username || !formik.values.email }
+                >Dodaj użytkownika </button>
+
             </form>
         </>
     )
